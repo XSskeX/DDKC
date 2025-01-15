8d2c12d28b062b0ef3f25bc83abf4dc4b1632853
Sske <1097484850@qq.com>
2025-01-15 21:09:18 +0800

    5th

---
+++ kv cache/ddkc/main.py
@@ -0,0 +1,26 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from safetensors import safe_open
+device = "cuda"  # the device to load the model onto
+model_name_or_path = "/home/LAB/lishuo/jerry/DDKC/quan/qwen2_1.5b_v2"
+# tokenizer_path = "/home/LAB/lishuo/jerry/DDKC/quan"
+model = AutoModelForCausalLM.from_pretrained(
+  model_name_or_path,
+  torch_dtype="auto",
+  device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+prefix = "北京是中国的首都"
+model_inputs = tokenizer([prefix], return_tensors="pt").to(device)
+
+generated_ids = model.generate(
+  model_inputs.input_ids,
+  max_new_tokens=400,
+  repetition_penalty=1.15
+)
+generated_ids = [
+  output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
